f56da7ecd9deef71d6b79d74948119a01dcc8f50
Avinash4332 <120744178+Avinash4332@users.noreply.github.com>
2022-12-17 01:31:39 +0530

    Create PFCOP.php

---
+++ PFCOP.php
@@ -0,0 +1,17 @@
+{
+"version" : 3,
+"Sd":"13109779",
+"Less" : true ,
+"Zero" : false ,
+"Shake" : false ,
+"MB" : false,
+"Aim" : false,
+"HS" : false,
+"Ipad" : true,
+"InstantHit" : true,
+"Cross" : true,
+"Xhit" : false,
+"Knock" : false,
+"SitScope" : false,
+"By" : false
+}
