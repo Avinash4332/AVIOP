8218ce65100d82ab97610f2cfb3bbea5aa351f20
Avinash4332 <120744178+Avinash4332@users.noreply.github.com>
2022-12-19 12:24:50 +0530

    Create TEST.php

---
+++ TEST.php
@@ -0,0 +1,17 @@
+{
+"version" : 3,
+"Sd":"12295114",
+"Less" : true ,
+"Zero" : false ,
+"Shake" : false ,
+"MB" : false,
+"Aim" : false,
+"HS" : false,
+"Ipad" : true,
+"InstantHit" : true,
+"Cross" : true,
+"Xhit" : false,
+"Knock" : false,
+"SitScope" : false,
+"By" : false
+}
