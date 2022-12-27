8e235ec5fc19863604dfaa11cb729249d6a903b9
Avinash4332 <120744178+Avinash4332@users.noreply.github.com>
2022-12-28 01:45:38 +0530

    Update Avi.lua

--- Avi.lua
+++ Avi.lua
@@ -1,7 +1,7 @@
            for (int i = 0; i < ?; i++) {
            SetSearchRange(OTHER);
-			MemorySearch(logic1, "varx", &v4, DWORD);
-			MemoryOffset(logic1, "vary", off[i], &v4, DWORD);
-			MemoryWrite(logic1, "varz", 0, DWORD);
+			MemorySearch(logic1, "future", &v4, DWORD);
+			MemoryOffset(logic1, "fucker", off[i], &v4, DWORD);
+			MemoryWrite(logic1, "fucked", 0, DWORD);
 			ClearResults();
 			}
