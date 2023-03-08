7c9a80eafea34ada860b7fb9701a831627ea0f07
Avinash4332 <120744178+Avinash4332@users.noreply.github.com>
2023-03-08 11:23:21 +0530

    Create PFC.php

---
+++ PFC.php
@@ -0,0 +1,17 @@
+{
+"version" : 9,
+"Sd":"8373495",
+"Less" : true ,
+"Zero" : false ,
+"Shake" : false ,
+"MB" : true,
+"Aim" : false,
+"HS" : true,
+"Ipad" : true,
+"InstantHit" : true,
+"Cross" : true,
+"Xhit" : true,
+"Knock" : false,
+"SitScope" : true,
+"By" : true
+}
